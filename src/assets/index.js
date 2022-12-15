97538300e434543a3e7262fa9b89615e40ed50cf
Muhammed Emin Karaaaslan <mhmmdkaraaslan@gmail.com>
2022-12-15 22:20:32 +0300

    folder structre created

---
+++ src/assets/index.js
@@ -0,0 +1,42 @@
+export const firstColumn = [
+    require('./0.webp'),
+    require('./1.webp'),
+    require('./2.webp'),
+    require('./3.webp'),
+    require('./4.webp'),
+]
+
+export const secondColumn = [
+    require('./5.webp'),
+    require('./6.webp'),
+    require('./7.webp'),
+    require('./8.webp'),
+    require('./9.webp'),
+]
+
+
+export const thirdColumn = [
+    require('./10.webp'),
+    require('./11.webp'),
+    require('./12.jpg'),
+    require('./13.webp'),
+    require('./14.webp'),
+]
+
+
+export const fourthColumn = [
+    require('./15.webp'),
+    require('./16.webp'),
+    require('./17.webp'),
+    require('./18.webp'),
+    require('./19.webp'),
+]
+
+
+export const fifthColumn = [
+    require('./20.webp'),
+    require('./21.webp'),
+    require('./22.webp'),
+    require('./23.webp'),
+    require('./24.webp'),
+]
