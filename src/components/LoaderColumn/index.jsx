c118297d7b67ed37efc7c825fbf7b130f37053fd
Muhammed Emin Karaaaslan <mhmmdkaraaslan@gmail.com>
2022-12-16 00:06:17 +0300

    Loading animation created

---
+++ src/components/LoaderColumn/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.scss'
+
+const LoaderColumn = () => {
+  return (
+    <div>LoaderColumn</div>
+  )
+}
+
+export default LoaderColumn
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import LoadingSlider from '../../containers/LoadingSlider'
+
+import './style.scss'
+
+const Home = () => {
+  return (
+    <div>
+        <LoadingSlider />
+    </div>
+  )
+}
+
+export default Home
